bc3daf98fb9743a11975a2d10deb7a3cf60e1472
Darsh Gondalia <darshgondalia10@gmail.com>
2020-12-14 18:05:32 -0500

    initializing repository

---
+++ Tetris1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tetris1
+//
+//  Created by Darsh Gondalia on 12/05/20.
+//  Copyright © 2020 Darsh Gondalia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TetrisGameView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tetris1Tests/Tetris1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Tetris1Tests.swift
+//  Tetris1Tests
+//
+//  Created by Darsh Gondalia on 12/05/20.
+//  Copyright © 2020 Darsh Gondalia. All rights reserved.
+//
+
+import XCTest
+@testable import Tetris1
+
+class Tetris1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
